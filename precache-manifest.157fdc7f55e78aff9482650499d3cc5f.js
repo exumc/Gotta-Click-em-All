c737cae562f7469bdb9834b8eb0bd329843aec5a
exumc <cordexum@gmail.com>
2019-01-24 20:34:40 -0800

    Updates

---
+++ precache-manifest.157fdc7f55e78aff9482650499d3cc5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "155dd13792986d716b2e",
+    "url": "/Gotta-Click-em-All/static/js/runtime~main.155dd137.js"
+  },
+  {
+    "revision": "4fcf3240b03deff7eca2",
+    "url": "/Gotta-Click-em-All/static/js/main.4fcf3240.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Gotta-Click-em-All/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4fcf3240b03deff7eca2",
+    "url": "/Gotta-Click-em-All/static/css/main.246f173f.chunk.css"
+  },
+  {
+    "revision": "4bb3c7faf62848be17a674e59f4a1ed8",
+    "url": "/Gotta-Click-em-All/index.html"
+  }
+];
